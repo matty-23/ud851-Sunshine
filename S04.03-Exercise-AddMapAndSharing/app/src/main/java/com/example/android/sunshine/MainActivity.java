2ee493ed3c9c9950dc5616e41d561ca35685057d
Matthew Yang <mattyang@cs.washington.edu>
2018-01-31 11:07:41 -0800

    add map and share intents

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -20,6 +20,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -221,7 +222,27 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
             return true;
         }
 
-        // TODO (2) Launch the map when the map menu item is clicked
+        if(id == R.id.action_open_map) {
+
+            String address = "1600 Ampitheatre Parkway, CA";
+            Uri.Builder builder= new Uri.Builder();
+
+            builder.scheme("geo")
+                    .path("0,0")
+                    .query(address);
+
+            Uri uri = builder.build();
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+
+            intent.setData(uri);
+
+            if(intent.resolveActivity(getPackageManager()) != null) {
+
+                startActivity(intent);
+
+            }
+        }
 
         return super.onOptionsItemSelected(item);
     }
