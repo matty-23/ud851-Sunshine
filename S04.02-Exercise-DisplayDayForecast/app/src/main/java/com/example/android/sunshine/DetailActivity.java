e3f26f0b6c8e9d24a80c679809252835cc6ba4d7
Matthew Yang <mattyang@cs.washington.edu>
2018-01-31 10:24:22 -0800

    implement intent from MainActivity to DetailActivity to pass weather data

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,29 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    TextView weatherDataTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        weatherDataTextView = (TextView) findViewById(R.id.tv_weather_data);
+
+        Intent intent = getIntent();
+
+        if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+
+            weatherDataTextView.setText(intent.getStringExtra(Intent.EXTRA_TEXT));
+
+        }
     }
 }
