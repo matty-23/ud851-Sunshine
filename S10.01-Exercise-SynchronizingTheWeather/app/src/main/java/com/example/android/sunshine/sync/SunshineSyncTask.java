d15818ef5914d75e74f5b3fd37cd1f8f7be6269c
Matthew Yang <mattyang@cs.washington.edu>
2018-03-24 13:04:09 -0700

    create intent service to sync weather data

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,43 @@
-//  TODO (1) Create a class called SunshineSyncTask
-//  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
-//      TODO (3) Within syncWeather, fetch new weather data
-//      TODO (4) If we have valid results, delete the old data and insert the new
+
+package com.example.android.sunshine.sync;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.util.Log;
+
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+import com.example.android.sunshine.utilities.SunshineWeatherUtils;
+
+public class SunshineSyncTask {
+
+    synchronized public static void syncWeather(Context context) {
+        
+        try {
+
+            String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(NetworkUtils.getUrl(context));
+
+            ContentValues[] contentValuesWeatherResponse = OpenWeatherJsonUtils.getWeatherContentValuesFromJson(context, jsonWeatherResponse);
+
+            if(contentValuesWeatherResponse != null && contentValuesWeatherResponse.length > 0) {
+
+                ContentResolver weatherResolver = context.getContentResolver();
+
+                //remove old weather data
+                weatherResolver.delete(WeatherContract.WeatherEntry.CONTENT_URI, null, null);
+
+                //insert new weather data
+                weatherResolver.bulkInsert(WeatherContract.WeatherEntry.CONTENT_URI, contentValuesWeatherResponse);
+
+            }
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+    }
+}
